5b429d8a92c1188664019bfa1b7c823443aa38a7
Vitorbbs99 <vitorbbs99@gmail.com>
2026-02-18 09:58:28 -0300

    Adjustment/cleanCode

--- src/main/java/com/example/javaapi/coupon/domain/applicationservice/coupon/CleanCode.java
+++ src/main/java/com/example/javaapi/coupon/domain/applicationservice/coupon/CleanCode.java
@@ -3,9 +3,9 @@ package com.example.javaapi.coupon.domain.applicationservice.coupon;
 public record CleanCode (String code) {
     public CleanCode {
         String removeSpecialCharacters = code.replaceAll("[^a-zA-Z0-9]", "");
-        if (removeSpecialCharacters.length() != 6) {
+        if (removeSpecialCharacters.length() < 6) {
             throw new IllegalArgumentException("O código do cupom deve ter 6 caracteres alfanuméricos.");
         }
-        code = removeSpecialCharacters.toUpperCase();
+        code = removeSpecialCharacters.substring(0, 6).toUpperCase();
     }
 }
