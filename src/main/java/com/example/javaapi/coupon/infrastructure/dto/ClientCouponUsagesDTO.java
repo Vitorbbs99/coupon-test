9cc4669d7ae1d0c04f7880591ce02b8d66544334
Vitorbbs99 <vitorbbs99@gmail.com>
2026-07-18 12:52:20 -0300

    adjust/endpoint/list/coupon-usages

--- src/main/java/com/example/javaapi/coupon/infrastructure/dto/ClientCouponUsagesDTO.java
+++ src/main/java/com/example/javaapi/coupon/infrastructure/dto/ClientCouponUsagesDTO.java
@@ -1,19 +1,26 @@
 package com.example.javaapi.coupon.infrastructure.dto;
 
 import com.example.javaapi.coupon.domain.entity.CouponUsage;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientCouponUsagesDTO {
-    private final String id;
-    private final LocalDateTime createdAt;
+    private String id;
+    private LocalDateTime createdAt;
+    private CouponDTO couponDTO;
 
     public static ClientCouponUsagesDTO create(CouponUsage couponUsage) {
         return new ClientCouponUsagesDTO(
                 couponUsage.getId(),
-                couponUsage.getCreatedAt()
+                couponUsage.getCreatedAt(),
+                Optional.ofNullable(couponUsage.getCoupon()).map(CouponDTO::create).orElse(null)
         );
     }
 }
